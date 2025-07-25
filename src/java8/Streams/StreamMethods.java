45b873daaa1f64c9eb1881d2c827a1d4ac5af00b
Shreyash Magdum <shreyashmagdum1215@gmail.com>
2025-07-25 13:50:56 +0530

    25/7/25

--- src/java8/Streams/StreamMethods.java
+++ src/java8/Streams/StreamMethods.java
@@ -12,13 +12,29 @@ public class StreamMethods {
 
         //1.Filter(): used when we need to filterout some element based on the condition like True/False
         //suppose we have to find the names starts with S then we use filter for this task
-            //names.stream().filter(e->e.startsWith("S")).forEach(System.out::println);
+        names.stream().filter(e->e.startsWith("S")).forEach(System.out::println);
         //same with letter D
-            //names.stream().filter(e->e.startsWith("D")).forEach(System.out::println);
+        names.stream().filter(e->e.startsWith("D")).forEach(System.out::println);
             //suppose we need only even numbers from the list
-            //numbers.stream().filter(e->e%2==0).forEach(e-> System.out.print(e+" "));
+        numbers.stream().filter(e->e%2==0).forEach(e-> System.out.print(e+" "));
 
         //2.Map(): used when we need to do operation on each item in collection like squaring the integer like that
+        numbers.stream().map(e->e*e).forEach(e-> System.out.print(e+" "));
+
+
+        //3.sorted(): as name suggests it used to sort the elements
+        numbers.stream().sorted().forEach(e-> System.out.print(e+" "));
+        names.stream().sorted().forEach(e-> System.out.println(e+" "));
+
+
+        //4.min() and max(): used to collect the minimum nad maximum element from collection, it uses comparator
+        //to compare the element
+        Integer minElement = numbers.stream().min((a, b) -> a.compareTo(b)).get();
+        System.out.println(minElement);
+
+        Integer maxElement = numbers.stream().max(Integer::compareTo).get();
+        System.out.println(maxElement);
+
 
     }
 }
